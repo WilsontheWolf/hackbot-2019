941c32e7e13e8aab0c91da7a14b097f2f12cfed3
Khoo Hao Yit <40757009+KhooHaoYit@users.noreply.github.com>
2019-06-29 15:31:47 +0800

    Add user_info

---
+++ commands/user_info.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+exports.run = async (client, $, message, args) => {
+  const user = await client.fetchUser(args);
+  const embed = new Discord.RichEmbed()
+    .setDescription(`Information about ${user}`)
+    .setColor('RANDOM')
+    .setThumbnail(user.displayAvatarURL)
+    .addField('Bot', user.bot, true)
+    .addField('Id', user.id, true)
+    .addField('Status', user.presence.status, true)
+    .addField('Tag', user.tag, true);
+  const member = message.guild.members.get(user.id);
+  if(member){
+    embed.addField('Joined at', member.joinedAt, true)
+      .addField('Nickname', member.nickname, true)
+      .addField('Roles', member.roles.array().join(' ||1|| '), true);
+  }
+  message.channel.send(embed);
+};
+
+exports.config = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['user'],
+  botOwnerOnly: false,
+  hidden: false
+};
+
+exports.info = {
+  name: "user_info",
+  category: "Miscellaneous",
+  description: 'Information about user',
+  usage: "user_info <id>"
+};
